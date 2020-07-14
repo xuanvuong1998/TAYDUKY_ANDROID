a13b01f3f0e99234219fab24842969d033a836da
VuongHo <hoxuanvuong1998@gmail.com>
2020-07-14 19:11:07 +0800

    Fix image layout in ROles fragment

--- app/src/main/java/vuong/hx/tayduky/remote/services/SceneRoleService.java
+++ app/src/main/java/vuong/hx/tayduky/remote/services/SceneRoleService.java
@@ -1,7 +1,5 @@
 package vuong.hx.tayduky.remote.services;
 
-import java.util.List;
-
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
@@ -12,15 +10,12 @@ import retrofit2.http.POST;
 import retrofit2.http.PUT;
 import retrofit2.http.Path;
 import vuong.hx.tayduky.models.SceneRole;
-import vuong.hx.tayduky.models.SceneRoleFullInfo;
 import vuong.hx.tayduky.remote.api.ApiConfig;
 
 public interface SceneRoleService {
 
     @GET(ApiConfig.Apis.SceneRole.GET_BY_ID)
     Call<SceneRole> getById(@Path("sceneRole_id") int sceneRoleId);
-
-
 
     @POST(ApiConfig.Apis.SceneRole.CREATE)
     Call<ResponseBody> createNew(@Header("Authorization") String token,  @Body SceneRole SceneRole);
